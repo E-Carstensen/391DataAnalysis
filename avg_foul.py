4dc8f4f5ed4ce1a85c36f98b9334a05992c6e9ca
Eric <ecarstensen99@gmail.com>
2024-04-08 11:02:09 -0600

    changes

--- avg_foul.py
+++ avg_foul.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv("Final.csv")
 
 data['HomeShotsOnTarget_Discrete'] = pd.qcut(data['HomeShotsOnTarget'], q=3, labels=['LowHomeShotsOnTarget', 'MediumHomeShotsOnTarget', 'HighHomeShotsOnTarget'])
 data['AwayShotsOnTarget_Discrete'] = pd.qcut(data['AwayShotsOnTarget'], q=3, labels=['LowAwayShotsOnTarget', 'MediumAwayShotsOnTarget', 'HighAwayShotsOnTarget'])
@@ -10,7 +10,11 @@ data['AwayShots_Discrete'] = pd.qcut(data['AwayShots'], q=3, labels=['LowAwaySho
 
 data['HomeFouls_Discrete'] = pd.qcut(data['HomeFouls'], q=3, labels=['LowHomeFouls', 'MediumHomeFouls', 'HighHomeFouls'])
 data['AwayFouls_Discrete'] = pd.qcut(data['AwayFouls'], q=3, labels=['LowAwayFouls', 'MediumAwayFouls', 'HighAwayFouls'])
+
+data['HomeReds_Discrete'] = pd.qcut(data['HomeReds'], q=3, labels=['LowHomeReds', 'MediumHomeReds', 'HighHomeReds'])
+data['AwayReds_Discrete'] = pd.qcut(data['AwayReds'], q=3, labels=['LowAwayReds', 'MediumAwayReds', 'HighAwayReds'])
+
 data['Result_Discrete'] = data['Result'].map({'H': 1, 'A': -1, "D": 0})
 
 
-data.to_csv("simplified.csv")
+data.to_csv("Final.csv")
--- clutsering.py
+++ clutsering.py
@@ -15,14 +15,17 @@ import numpy as np
 df = df.replace('NaN', np.nan)
 
 df = df.dropna()
-
+pd.set_option('display.max_columns', None)  # Show all columns
+#pd.set_option('display.max_rows', None)
+#df_encoded = pd.get_dummies(df, columns=['HomeTeam', 'AwayTeam'])
+#X = df.drop(columns=['HomeTeam', 'AwayTeam','Index', 'Div', 'Date', 'Time', 'HomeGoals', 'AwayGoals', 'Result', 'HalfTimeHG', 'HalfTimeAG', 'HalfTimeResult', 'HomeCorners', 'AwayCorners', 'HomeYellows', 'AwayYellows', 'HomeReds', 'AwayReds', 'HomeShotsOnTarget_Discrete', 'AwayShotsOnTarget_Discrete', 'HomeShots_Discrete', 'AwayShots_Discrete', 'HomeFouls_Discrete', 'AwayFouls_Discrete', 'Result_Discrete', 'Result_Encoded'])
 # Define features and target variable
-X = df[['HomeShots', 'HomeShotsOnTarget', 'HomeFouls', 'AwayShots', 'AwayShotsOnTarget', 'AwayFouls',"HomeTeam", "AwayTeam"]]  # Features
+X = df[['HomeShots', 'HomeShotsOnTarget', 'HomeFouls', 'AwayShots', 'AwayShotsOnTarget', 'AwayFouls','HomeCorners', 'AwayCorners', 'HomeYellows', 'AwayYellows', 'HomeReds', 'AwayReds']]  # Features
 y = df['Result_Discrete']  # Target variable
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+print(['HomeShots', 'HomeShotsOnTarget', 'HomeFouls', 'AwayShots', 'AwayShotsOnTarget', 'AwayFouls','HomeCorners', 'AwayCorners', 'HomeYellows', 'AwayYellows', 'HomeReds', 'AwayReds'])
 # Initialize and train the Linear Regression model
 model = LinearRegression()
 model.fit(X_train, y_train)
